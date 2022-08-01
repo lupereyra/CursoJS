1c4861a84c265647648daf504d46f594d0e1fd28
Luciana Pereyra <lucianapereyra82@gmail.com>
2022-08-01 20:05:24 -0300

    Simulador interactivo

--- js/index.js
+++ js/index.js
@@ -1,23 +1,62 @@
-//uso del FOR
-//suma la cantidad de numeros naturales que el usuario elija
-let sum = 0
-let numero = prompt("Elija un numero del 1 al 100")
-for (let i = 1; i <= numero; i++) {
-    sum += i; 
+//Balneario
+
+let servicioElejido
+function mostrarServicios(){ 
+    alert("Bienvenido al balneario El Soleado!")
+    let servicios = prompt("Que servicio desea adquirir hoy? \n1)Alquiler de carpa \n2)Alquiler de sombrilla")
+    while(servicios!= 1 && servicios!= 2){
+        alert("La opcion ingresada es incorrecta")
+        servicios = prompt("Que servicio desea adquirir hoy? \n1)Alquiler de carpa \n2)Alquiler de sombrilla")
+    } 
+    let periodo = prompt("¿Que periodo de tiempo queres cotizar? \n1)Temporada completa \n2)Mes\n3)Quincena\n4)Dia")
+    while(periodo!= 1 && periodo!= 2 && periodo!= 3 && periodo!= 4){
+        alert("La opcion ingresada es incorrecta")
+        periodo = prompt("¿Que periodo de tiempo queres cotizar? \n1)Temporada completa \n2)Mes\n3)Quincena\n4)Dia")
+    }
+    
+    if(servicios == 1){
+        servicioElejido = "carpa"
+
+    } else{
+        servicioElejido = "sombrilla"
+
+    }
+    switch(periodo){
+        case "1":
+            return "temporada"
+        case "2":
+            return "mes"
+        case "3":
+            return "quincena"
+        case "4":
+            return "dia"        
+    }
 }
 
-console.log("La suma de los numeros naturales hasta el", numero, "es de ", sum)
 
-
-//uso del WHILE
-//suma todos los numeros positivos que el usuario ingrese, cuando el mismo ingresa uno negativo, el ciclo se corta.
-let suma = 0
-let number = parseInt(prompt('Ingrese un numero: '))
-
-while(number >= 0) {
-    suma += number
-    number = parseInt(prompt('Ingrese un numero: '));
+function tarifaCotizada(servicios, periodo){
+    if(servicios === "carpa" && periodo ==="dia"){
+        return 4000
+    }else if(servicios === "carpa" && periodo ==="quincena"){
+        return 55000
+    }else if(servicios === "carpa" && periodo ==="mes"){
+        return 100000       
+    }else if(servicios === "carpa" && periodo ==="temporada"){
+        return  300000 
+    }else if(servicios === "sombrilla" && periodo ==="dia"){
+        return  1800
+    }else if(servicios === "sombrilla" && periodo ==="quincena"){
+        return 25000
+    }else if(servicios === "sombrilla" && periodo ==="mes"){
+        return 45000 
+    }else {
+        return 160000
+    }
+        
 }
 
 
-console.log("La suma de los numeros ingresados es ", suma)
+let periodoSeleccionado = mostrarServicios()
+let tarifaFinal = tarifaCotizada(servicioElejido, periodoSeleccionado)
+
+alert("La tarifa final correspondiente a una " + servicioElejido + " por " + periodoSeleccionado + " es de : $"+ tarifaFinal +" (IVA incluido)")
